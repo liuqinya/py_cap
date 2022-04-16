7bcb49e81733790604f9a91e04094e457cf1dd52
liuqinya <liuqinya@gmail.com>
2022-04-15 21:56:07 -0400

    add a utility script to get a list of distances from the sac file

---
+++ dist_list_from_sacfiles.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+import sys
+from obspy import read
+
+# read input arguments
+if len(sys.argv) <= 1:
+    raise BaseException('input sac files')
+
+dist_list=[]
+az_list=[]
+# loop over events
+for sacfile in sys.argv[1:]:
+    st=read(sacfile)  # sac header goes into st[0].stats.sac
+    tr=st[0]
+    if hasattr(tr.stats.sac, 'dist') and hasattr(tr.stats.sac,'az'):
+        dist_list.append(tr.stats.sac.dist)
+        az_list.append(tr.stats.sac.az)
+    else:
+        raise BaseException('Problem inquiring dist and az header of file'+sacfile)
+    #print(("%s   %.3f   %.3f") %(sacfile, dist, az))
+
+dist_list = sorted(set(dist_list))
+print(' '.join(map(str,dist_list)))
+
+
+
+    
